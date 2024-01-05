f46c6eed8977ab346f0e895e6d208e0f3b7189f7
Roshan-Farkate <138759751+Roshan-Farkate@users.noreply.github.com>
2024-01-05 17:14:52 +0530

    Update WeatherForecastController.cs

--- dotnetproject/Controllers/WeatherForecastController.cs
+++ dotnetproject/Controllers/WeatherForecastController.cs
@@ -6,6 +6,8 @@ namespace dotnetproject.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        public string Climate;
+        
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
