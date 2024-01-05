c77462e3104f359f519c2f39d31f545a8b715c07
Roshan-Farkate <138759751+Roshan-Farkate@users.noreply.github.com>
2024-01-05 17:41:27 +0530

    Update WeatherForecastController.cs

--- dotnetproject/Controllers/WeatherForecastController.cs
+++ dotnetproject/Controllers/WeatherForecastController.cs
@@ -1,4 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace dotnetproject.Controllers
 {
@@ -6,8 +10,8 @@ namespace dotnetproject.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        public string Climate;
-        
+        public string Climate; // This should be a property, not a public field
+
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -23,6 +27,9 @@ namespace dotnetproject.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            // Intentional error: Accessing a property on a null object
+            var climateLength = Climate.Length; // This will throw a null reference exception
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
